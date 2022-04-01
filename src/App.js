1fdabb928bfea39272f4d75fc99417f8a8c87232
OmarRaihan <omarraihan91@gmail.com>
2022-04-01 23:59:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MyLineChart from './components/MyLineChart/MyLineChart';
+import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring';
 
 function App() {
+
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <animated.div style={props}>I will fade in</animated.div>
+      <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
     </div>
   );
 }
